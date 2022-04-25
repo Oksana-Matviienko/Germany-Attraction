24c97da0cf9d802f85f5ed07503dffc2a70ae422
Oksana <7564211@gmail.com>
2022-04-25 17:32:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+  const [place, setPlace] = useState(0);
+  const {id, title, image, description, website} = data[place];
+  const [showMore, setShowMore] = useState(false);
+ const previousPlace = () =>{
+   setPlace((place=>{
+     place--;
+     if (place < 0){
+       return data.length-1;
+     }
+     return place;
+   }))
+ }
+ const nextPlace =() =>{
+   setPlace ((place =>{
+     place++;
+     if (place > data.length -1){
+       place = 0;
+     }
+     return place;
+   }))
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+      <h1> {data.length} places to visit in Germany </h1>
+      </div>
+      <div className="container">
+<h2>{id} - {title}</h2>
+      </div>
+      <div className="container">
+<img src={image} width="600px" height="400px" alt="Germany seesigths"/>
+      </div>
+      <div className="container description">
+<p>{showMore ? description : description.substring(0,300)+ "..."}
+<button className="btnshow" onClick={()=>setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"}</button></p>
+      </div>
+      <div className="container">
+<p> Website: {website}</p>
+    </div>
+    <div className="btn">
+    <button onClick={previousPlace}>Previous</button>
+    <button onClick={nextPlace}>Next</button>
+    </div>
+    <div className="container education">
+      <p>All information was taken from the website https://www.tripzaza.com/en/destinations/dostoprimechatelnosti-germanii
+for educational purposes only </p>
+    </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,115 @@
+export const data =[
+   {
+      id: 1,
+      title: "Cologne Dom",
+     image: "https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-1-Cologne-Cathedral-e1492727734625.jpg",
+     description: `Cologne Cathedral has become the most important landmark in Germany not only because of its appearance, but also because of its rich history.
+     As early as the first century A.D., a temple dedicated to the Roman gods was erected on the site of the cathedral. From the 4th century onwards, Christian churches began to be built on the site, but they were inevitably destroyed by fires. And in the middle of the 13th century, after the relics of the three Magi were given to the Archbishop of Cologne, it was decided to build a temple. And in 1248, the first stone was laid in the foundation of the Cologne Cathedral. Construction of the cathedral dragged on for centuries - by 1560, only its foundation was laid. Construction was not resumed until 1824, when according to plans found in archives the tall towers and facades were completed. The completion of the construction was celebrated in 1880.
+     Today the Cologne Cathedral fascinates not only with its facades but also with its interior. The main hall is decorated with a variety of figures of saints, carved columns, the walls and floor are decorated with mosaics and gilding. Sacred awe is aroused by an oak cross, faithfully reproducing the crucifixion of Christ. In short, every corner here is imbued with its history and one can feel the atmosphere of the Middle Ages, especially during organ music concerts.`,
+     website: "https://www.koelner-dom.de"
+   },
+   {
+      id: 2,
+      title: "Schloss Neuschwanstein",
+      image: "https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-2-Neuschwanstein-Castle-e1492727971928.jpg",
+      description:`The first time one looks at the facade of Neuschwanstein Castle from afar, one associates this amazingly beautiful structure with a toy. The beauty of the neat ivory towers, topped by pointed emerald domes, seems so implausible. Even more fabulous gives an incredibly beautiful natural background in the form of alpine slopes, buried in the green forests. Neuschwanstein castle can safely be included in the rating of the most beautiful places in the world!
+      This breathtaking landmark of Germany appeared on the initiative of the Bavarian King Ludwig II, who embodied in the castle his fantastic visions and romantic ideas. The first stone was laid in the foundation of the future castle in 1869. Initially set three years construction term was postponed more than once: labor input of the project, location of the site in the mountains, as well as constant adjustments, which were made in the project by the monarch himself.
+      Just like the facade of the castle, the interior furnishings reflected the monarch's fantasy dreams:
+      the decoration of the castle halls embodied various plots of Germanic mythology, the walls and ceiling of the rooms are crowned by numerous elements of painting;
+      the king's bedroom in neo-Gothic style, with its beautifully carved decorations and high reliefs;
+      the prayer room, created in the memory of St. Louis, King of France, hosts a casement altar decorated with a refined carving;
+      The image of the swan knight Lohengrin is at the base of the drawing room decorations, and the swan theme can be easily discerned in the carved decoration of the wooden elements and the gold embroidery of the curtains.
+      Translated with www.DeepL.com/Translator (free version)`,
+      website:"http://www.schloesser.bayern.de"
+   },
+   {
+      id: 3,
+      title: "Brandenburger Tor",
+      image: "https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-3-The-Brandenburg-Gate-e1492728193602.jpg",
+      description:`If you are wondering what is the true symbol of Germany and what you should see in Germany first, you should definitely see the Brandenburg Gate. This is a truly legendary structure, impressive in its size, architecture and symbolizing the most important milestones in the country's history.
+      Thus, the Peace Gate, as it was originally called, was laid in 1789, and the works of ancient builders were taken as the ideological basis of the project.
+      In 1871, the year of the formation of the German Empire around the kingdom of Prussia, the victorious soldiers marched through the Brandenburg Gate;
+      in 1918 and 1920, troops of counter-revolutionary soldiers had already passed through the arch;
+      in 1945, the red banner of the Russian victors flew on the quadriga of the gate, and Stalin's portrait was hoisted beneath it;
+      in 1961, the gate was closed and built into the Berlin Wall, dividing Germany into two states - capitalist and socialist;
+      in 1989, when the Berlin Wall collapsed, the gate reopened, since then having become a symbol of peace and national unity.
+      The gate is an almost complete replica of the arch of the Propylaeum on the Parthenon. Its total height is 26 meters, the construction has 6 pillars and 5 corridors, the main of which is designed for the passage of solemn corteges.
+      Translated with www.DeepL.com/Translator (free version)`,
+      website: "No information found"
+   },
+   {
+      id: 4,
+      title:`Schwarzwald`,
+      image:"https://www.tripzaza.com/ru/destinations/wp-content/uploads/2020/01/4-black_forest.jpg",
+      description:`n the south of the country, in the Black Forest region, there is a truly fairy-tale landmark in Germany - the Black Forest, which brings back memories of the characters from the famous fairy tales of the Brothers Grimm. Most of the forest is made up of dense and tall spruces, hence the name Black Forest.
+      In the shade of the dense forest, streams are ringing, rivers are flowing and whole waterfalls are murmuring. The Danube River, which flows through a number of European countries, has its source in this forest. Deep lakes, huge old spruces, boulders overgrown with moss - everything here seems to keep its special fairy tale secrets.
+      For example, the waterfalls of the black forest are considered the highest in the country, and some of them attract streams of tourists.
+      Today the black forest is not as dense as it used to be: there are equipped ski resorts, recreation areas and even an amusement park. The water in the lakes of the Black Forest is crystal clear and many divers come here for diving - the deep lakes Schluchsee and Titisee are especially popular. Equally popular are motorboat rides on the mirror-like lake surface.
+      Locals as well as visitors often take mountain hikes here - such hikes are well worth the effort. The many well-trodden trails up here allow you to enjoy the local scenery to the full, and at a certain altitude, you'll be able to see an extremely scenic view of the surrounding countryside.`,
+      website:"No information found"
+   },
+   {
+      id: 5,
+      title: `Spreeinsel`,
+      image:"https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-5-Museum-Island-of-Berlin-e1492728590515.jpg",
+      description:`To choose what to see in Germany, you don't have to go far - right in the middle of the European capital, on the small island of Spreeinsel, is one of the most striking and impressive cultural heritage sites in the country - the so-called Museum Island, which is a whole museum complex, including five museums, founded at different times.
+      The history of this complex dates back to 1797, when the idea of creating a museum on the island of Spreeinsel was born to exhibit antique collections and art objects from the Middle Ages.
+      The Old Museum - The Old Museum was the first to establish a museum complex on the island in 1830. Here the rarest collections of ancient Roman and ancient Greek art have found their place.
+      Bode Museum - erected between 1897-1904, the museum includes collections of Byzantine art and numismatic collections of coins from the 7th century B.C. to the present.
+      The Pergamon Museum, first opened in 1930, includes the Near Eastern Museum, the Museum of Islamic Art, and the Ancient Collection, with the Pergamon Altar, the Babylonian Gate, and the Mshatta Frieze among its most valuable exhibits.
+      The Old National Gallery - opened in 1876, it has an extensive collection of 19th-century paintings and presents works in the styles of Classicism, Impressionism, Romanticism, Modernism, and Biedermeier.
+      The new museum was founded in 1855 and is essentially a continuation of the collection of the Old Museum, which was so large that it required the opening of several more exhibition halls.`,
+      website:"No information found"
+   },
+   {
+      id: 6,
+      title:`Reichstagsgebäude`,
+      image: "https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-9-The-Reichstag-building-e1492730054282.jpg",
+      description:`What associations does the mention of the Reichstag evoke? The bloodiest events, World War II, Adolf Hitler and the red banner flying over this building in 1945 as a sign of the Soviet Union's victory over Nazi Germany. However, time moves on, and some associations are gradually replacing others: today the Reichstag is increasingly presented as a symbol of modern and united Germany, a structure with a rich and complicated history.
+      This German landmark is located in the heart of the German capital, on Paris Square. In 1884, the first stone was laid in the foundations of the building and construction continued for the next 10 years.
+      In 1933, the structure was badly damaged by a fire that occurred there. The former independent communist Marinus van der Lubbe was found guilty at the time, but the Nazis blamed it all on the communists, which prompted a communist coup. The governing body of the Reichstag was dissolved and all meetings from then on were held in the Krol Opera House. For 12 years after that the Reichstag building was not used for its intended purpose, and in 1939 it became a military building.
+      In 1945 the building was badly damaged by the Soviet forces and in April of the same year the scarlet victory banner was hoisted over it.
+      After a partial reconstruction in 1973, the building was used as a historical museum, and only in 1995 was a complete reconstruction carried out, over which a glass dome and a viewing platform were built.
+      Today the Reichstag has become one of the most visited places in Germany - annually more than 13 million tourists from all over the world come here.`,
+      website:"http://www.bundestag.de/en/visittheBundestag"
+   },
+   {
+      id: 7,
+      title:`Königssee`,
+      image:'https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-11-The-Konigssee-----Royal-Lake-e1492730894268.jpg',
+      description:`If there is a question of what to see in Germany of natural attractions, perhaps the first thing worth seeing is the extraordinary beauty of Lake Königssee, located in the southeastern part of the country. It is not without reason that the lake is one of the most beautiful natural sites in Germany. The lake was formed from the melting waters of the glaciers that covered our planet during the last ice age, making it the cleanest lake in Europe. The area on which the lake lies is part of the Berchtesgaden National Park.
+      The breathtaking beauty of the surrounding nature, the splendor of the mountain peaks framing the lake, the crystal clear water, the rare plants and wildlife - the views of this lake is really fascinating. The lake itself has an elongated shape, covers an area of about 5 square kilometers, its depth is 190 meters and the total length of the shoreline is about 20 kilometers.
+      In addition to the main name, the lake has a second name - Bartolomeuze, received because of the church of St. Bartholomew located on its west shore, which has become the hallmark of the lake. Nearby you can also see the royal hunting castle and roe deer walking around nearby.
+      In the northern part of the lake there is an island with a picturesque grotto and an organized toboggan and bobsleigh track. In addition, there is a sculpture of St. John of Nepomuk, who is supposed to protect the area from the elements.
+      If there is a question of what to see in Germany of natural attractions, perhaps the first thing worth seeing is the extraordinary beauty of Lake Königssee, located in the southeastern part of the country. It is not without reason that the lake is one of the most beautiful natural sites in Germany. The lake was formed from the melting waters of the glaciers that covered our planet during the last ice age, making it the cleanest lake in Europe. The area on which the lake lies is part of the Berchtesgaden National Park.`,
+      website:"No information found"
+   },
+   {
+      id: 8,
+      title:`Stritzelmarkt`,
+      image:"https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-12-The-Striezelmarkt-in-Dresden-e1492731128337.jpg",
+      description:`Germany's oldest Christmas market, the Stritzelmarkt, is one of Europe's most famous fairs, held annually during the Christmas season from late November to December 24. Every year the Altmarkt square in the center of Dresden attracts millions of visitors to the fair, which has been held here since 1434, keeping all the traditions of the holiday intact.
+      What happens at the Stritzelmarkt during these days of pre-holiday buzz? German craftsmen and craftsmen from all over the world sell all kinds of different and unusual handicrafts, proving the highest level of craftsmanship. Crowning all this magnificence is a Christmas tree, as well as characters that have become national symbols of Christmas - a huge snowman, a miner, a prune man and a snapper.
+      Numerous refreshments are waiting for the guests here: traditional Dresden stollen, donuts, Saxon cheesecake, fried chestnuts, chocolate figures, sausages and shish kebabs, punch and mulled wine, and much more.
+      And, of course, there is a huge variety of souvenirs and Christmas decorations: Christmas pyramids, cairns, candelabras, made for the holiday by the most skilled craftsmen.
+      Have you ever visited the Stritzelmarkt? Then you have a lot to see in Germany at Christmas time: do not miss a great opportunity to communicate with friendly German people, to participate in competitions and entertainment and to plunge into the atmosphere of real winter magic.`,
+      website: "No information found"
+   },
+   {
+      id: 9,
+      title:"Hohenzollern",
+      image:"https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Germany-15-Hohenzollern-Castle-e1492731774798.jpg",
+      description:`The astonishingly beautiful Hohenzollern castle in Baden-Württemberg, 50 km south of Stuttgart, is often referred to as "castle in the clouds". This extraordinarily beautiful construction stands on top of a mountain, surrounded by mists and low clouds. And that is why it looks more like a vision from a fairy tale than a real structure. It is not for nothing that the Hohenzollern is considered one of the most beautiful sights in Germany.
+      Since the 11th century the castle was the residence of the Hohenzollern royal dynasty, who ruled Prussia in the Middle Ages. The building of the castle was repeatedly restored and refurbished, and the castle had its final appearance in the middle of the 19th century. At the base of the castle are two chapels, beer garden framed by a beautiful garden.
+      Today, tourists can stroll around the surroundings, as well as take a tour inside the castle. Inside, visitors are introduced to the history of the state in general and the history of the Hohenzollerns in particular. You will become acquainted with the castle's rich past as you walk through the rooms furnished with antique furniture and decorated with tapestries and artwork.
+      As you approach the castle, you will see it long before you arrive. Numerous visitors, of which there are up to 300 thousand a year, appreciate the beauty and the rich historical past of the castle.`,
+      website:"http://www.burg-hohenzollern.com"
+   },
+   {
+      id: 10,
+      title: "Berchtesgaden National Park",
+      image: "https://www.tripzaza.com/ru/destinations/wp-content/uploads/2019/01/25-Berchtesgaden_National_Park_Berchtesgaden-area-e1561150445727.jpg",
+      description: `Exploring the sights of Germany on the map, you can see that in the German part of the Alps stretches the territory of the Berchtesgaden National Park. This is the only protected area of the country, located in the mountains, but its beauty it makes up for dozens of other natural sites. The mirror-like turquoise lakes, alpine meadows, wooded slopes, steep cliffs with a bottomless blue sky - all the most beautiful things in the world are concentrated in this park. On the area of more than 200 sq. km you can not only take a walk and admire the views, but also visit some interesting objects - for example, the deepest mountain lake Konigsi in the Alps, the world-famous St. Bartholomew Monastery, the Jenner mountain with beautiful views of the valleys, the mysterious Berchtesgaden salt mine. Throughout the reserve there is an extensive network of hiking trails through the most beautiful places of the park. Beside Germany is a country with an imperial history, mountain villages and alpine landscape - Austria.`,
+      website:"No information found"
+   },
+]
